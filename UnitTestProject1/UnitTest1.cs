84962156edcb822172e3c21116c0450d89e07ecf
wizer <147315905+ProgWizer@users.noreply.github.com>
2026-03-27 00:39:33 +0300

    update

--- UnitTestProject1/UnitTest1.cs
+++ UnitTestProject1/UnitTest1.cs
@@ -7,28 +7,65 @@ namespace UnitTestProject
     [TestClass]
     public class UserTests
     {
+        /* --- ТЕСТИРОВАНИЕ АВТОРИЗАЦИИ (EnterPage) --- */
+
         [TestMethod]
-        public void AuthTest_EmptyFields_ReturnsFalse()
+       
+        public void AuthTestSuccess()
         {
             var page = new EnterPage();
-            bool result = page.Auth("", "");
-            Assert.IsFalse(result);
+            bool result = page.Auth("qwe", "qwe");
+            Assert.IsTrue(result, "Ожидался успешный вход (PASS)");
+        }   
+
+        [TestMethod]
+       
+        public void AuthTestFail_WrongPassword() 
+        {
+            var page = new EnterPage();
+           
+            bool result = page.Auth("user", "123456");
+            Assert.IsFalse(result, "Вход должен быть отклонен при неверном пароле");
         }
 
         [TestMethod]
-        public void RegisterTest_DifferentPasswords_ReturnsFalse()
+       
+        public void AuthTestFail_EmptyFields() 
+        {
+            var page = new EnterPage();
+           
+            bool result = page.Auth("", "");
+            Assert.IsFalse(result, "Вход невозможен с пустыми полями");
+        }
+
+
+        /* --- ТЕСТИРОВАНИЕ РЕГИСТРАЦИИ (AuthPage) --- */
+
+        [TestMethod]
+       
+        public void RegisterTestSuccess() 
         {
             var page = new AuthPage();
-            bool result = page.Register("test", "123", "321");
-            Assert.IsFalse(result);
+            bool result = page.Register("new_user_777", "1234", "1234");
+            Assert.IsTrue(result, "Регистрация должна быть успешной"); 
         }
 
         [TestMethod]
-        public void AuthTest_ValidUser_ReturnsTrue()
+       
+        public void RegisterTestFail_DifferentPasswords() 
         {
-            var page = new EnterPage();
-            bool result = page.Auth("admin", "admin");
-            Assert.IsTrue(result);
+            var page = new AuthPage();
+            bool result = page.Register("test_user", "test123", "test1234");
+            Assert.IsFalse(result, "Ожидалась ошибка: Пароли не совпадают"); 
+        }
+
+        [TestMethod]
+       
+        public void RegisterTestFail_EmptyLogin() 
+        {
+            var page = new AuthPage();
+            bool result = page.Register("", "123", "123");
+            Assert.IsFalse(result, "Регистрация без логина должна быть невозможна"); 
         }
     }
 }
